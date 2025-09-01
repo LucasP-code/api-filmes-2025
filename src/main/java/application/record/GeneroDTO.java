7bb5205033ce58063586a5817562080bb57148a2
Lucas P. Pereira <lucaspandian2017@gmail.com>
2025-09-01 17:51:52 +0000

    aula01/09/2025

---
+++ src/main/java/application/record/GeneroDTO.java
@@ -0,0 +1,9 @@
+package application.record;
+
+import application.model.Genero;
+
+public record GeneroDTO(long id, String nome) {
+    public GeneroDTO(Genero dados) {
+        this(dados.getId(), dados.getNome());
+    }
+}
