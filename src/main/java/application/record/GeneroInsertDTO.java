c12f614f48868af686e0de8ee6a107a07983c07b
Lucas P. Pereira <lucaspandian2017@gmail.com>
2025-09-02 20:08:52 +0000

    Aula02/09/2025

--- src/main/java/application/record/GeneroInsertDTO.java
+++ src/main/java/application/record/GeneroInsertDTO.java
@@ -1,5 +1,10 @@
 package application.record;
 
+import application.model.Genero;
+
 public record GeneroInsertDTO(String nome)  {
-    
+    public GeneroInsertDTO(Genero dados){
+        this(dados.getNome());
+    }
+
 }
